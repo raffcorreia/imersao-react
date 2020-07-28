0a088b33a74ea0e0f213c141d97c9bd06032df13
Rafael Correia <raffcorreia@gmail.com>
2020-07-27 23:52:48 -0400

    initial commit with clean helloworld project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+        <h1>
+            Hello World!
+        </h1>
+        Testing the hot reload...
+    </div>
+  );
+}
+
+export default App;
