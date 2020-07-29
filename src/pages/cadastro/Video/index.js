c8651cc29f2a364425f2c9c2a82410926ebccbaf
Rafael Correia <raffcorreia@gmail.com>
2020-07-29 15:54:24 -0400

    add a page default as a masterpage with header and footer

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 function CadastroVideo() {
     return (
-        <div>
-            Página de Cadastro de Video
-        </div>
+        <PageDefault>
+            <h1>Cadastro de Video</h1>
+
+            <Link to="/cadastro/categoria">
+                Cadastro Categoria
+            </Link>
+        </PageDefault>
     )
 }
 
