2b968653990970dd1484adca4bf60d02a1942a43
Rafael Correia <raffcorreia@gmail.com>
2020-07-29 15:01:57 -0400

    Add Route to the project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+const page404 = () => (<div>oops this is a 404 ... <br/>:'(</div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={App} exact />
+            <Route component={page404} />
+        </Switch>
+    </BrowserRouter>,
+
   document.getElementById('root')
 );
  
