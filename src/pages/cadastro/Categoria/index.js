df8d3f23a517ce1bf555da86b7f1bed33bf2ca08
Rafael Correia <raffcorreia@gmail.com>
2020-07-30 15:44:30 -0400

    Add extra fields to category

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,33 +3,86 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+    const valoresInicias = {
+        nome: '',
+        descricao: '',
+        cor: ''
+    }
+
     const [categorias, setCategorias] = useState(['Test']);
-    const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
+    const [values, setValues] = useState(valoresInicias);
     
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value
+        })
+    }
+
+    function handleChange(e) {
+        setValue(
+            e.target.getAttribute('name'), 
+            e.target.value
+        );
+    }
+
+    // function handleChange(e) {
+    //     const { getAttribute, value } = e.target;
+    //     setValue(
+    //         getAttribute('name'), 
+    //         value
+    //     );
+    // }
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+            <h1>Cadastro de Categoria: {values.nome}</h1>
 
             <form 
                 onSubmit={function handleSubmit(e) {
                     e.preventDefault();
                     setCategorias([
                         ...categorias,
-                        nomeDaCategoria
+                        values
                     ]);
+
+                    setValues(valoresInicias);
                 }}
             >
+                <div>
+                    <label>
+                        Nome da Categoria:
+                        <input
+                            type="text"
+                            name="nome"
+                            value={values.nome}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Descrição:
+                        <textarea
+                            type="text"
+                            name="descricao"
+                            value={values.descricao}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            name="cor"
+                            value={values.cor}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
 
-                <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
-                        value={nomeDaCategoria}
-                        onChange={function handlerFunction(e) {
-                            setNomeDaCategoria(e.target.value);
-                        }}
-                    />
-                </label>
 
                 <button>
                     Cadastrar
@@ -40,7 +93,7 @@ function CadastroCategoria() {
                 {categorias.map((categoria, i) => {
                     return (
                         <li key={`${categoria}${i}`}>
-                            {categoria}
+                            {categoria.nome}
                         </li>
                     )
                 })}
