fbe0be8e026c6986dd2b0b6f05ed435d83cba023
Rafael Correia <raffcorreia@gmail.com>
2020-08-11 23:47:09 -0400

    add getAll to category repo, add category id match to video create

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import useForm from '../../../hooks/useForm';
 import Button from '../../../components/Button';
 import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
   const { handleChange, values } = useForm({
     titulo: 'Video padrão',
     url: 'https://www.youtube.com/watch?v=EcqLX0o-Erc',
     categoria: 'Front End',
   });
+
+  useEffect(() => {
+    categoriasRepository
+      .getAll()
+      .then((categoriasFromServer) => {
+        setCategorias(categoriasFromServer);
+      });
+    setCategorias();
+  }, []);
 
   return (
     <PageDefault>
@@ -21,10 +32,14 @@ function CadastroVideo() {
       <form onSubmit={(event) => {
         event.preventDefault();
 
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === values.categoria;
+        });
+
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
-          categoriaId: 1,
+          categoriaId: categoriaEscolhida.id,
         })
           .then(() => {
             console.log('Video cadastrado com sucesso!');
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,9 +1,21 @@
 import config from '../config';
 
-const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias?_embed=videos`;
+const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
+
+function getAll() {
+  return fetch(URL_CATEGORIES)
+    .then(async (resp) => {
+      if (resp.ok) {
+        const resposta = await resp.json();
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados :(');
+    });
+}
 
 function getAllWithVideos() {
-  return fetch(URL_CATEGORIES)
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (resp) => {
       if (resp.ok) {
         const resposta = await resp.json();
@@ -16,4 +28,5 @@ function getAllWithVideos() {
 
 export default {
   getAllWithVideos,
+  getAll,
 };
